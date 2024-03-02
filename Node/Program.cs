1d766349982e96f13c205e167b2717b35b7a25bd
Lukash Hr <lukasz.hr@outlook.com>
2024-03-02 11:45:36 +0100

    Node project was added.

---
+++ Node/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/feature/{name}/state", (HttpRequest request) =>
+    {
+        var featureName = request.RouteValues["name"];
+        return true;
+    })
+    .WithName("GetFeatureState")
+    .WithOpenApi(operation => new(operation)
+    {
+        Summary = "Allow checking feature state in real time",
+        Description = "Client can provide feature name and in response is feature state information"
+    });
+
+app.Run();
