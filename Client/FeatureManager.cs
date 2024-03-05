d842a3b06993f1461c7b4f94ede5e08ccbdfef9a
Lukash Hr <lukasz.hr@outlook.com>
2024-03-05 20:51:47 +0100

    Add first version of featureManager

---
+++ Client/FeatureManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Wookashi.FeatureSwitcher.Abstraction;
+using Wookashi.FeatureSwitcher.Abstraction.Models;
+
+namespace Wookashi.FeatureSwitcher.Client;
+
+public class FeatureManager : IFeatureManager
+{
+    private bool _alreadyRegistered;
+    public void RegisterFeatures(List<FeatureStateModel> features)
+    {
+        _alreadyRegistered = true;
+     //   throw new System.NotImplementedException();
+    }
+
+    public bool IsFeatureEnabled(string featureName)
+    {
+        return true;
+    }
+}
