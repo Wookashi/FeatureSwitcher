b5fe955d40f98f53147e10026ce293b27911b99f
Lukash Hr <lukasz.hr@outlook.com>
2024-03-05 20:48:00 +0100

    Add Console Client project.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Spectre.Console;
+using Wookashi.FeatureSwitcher.Abstraction;
+using Wookashi.FeatureSwitcher.Abstraction.Models;
+using Wookashi.FeatureSwitcher.Client;
+
+
+var featureCollection = new List<FeatureStateModel>
+{
+    new("Foo",true),
+    new("Bar", false),
+    new("Baz", true),
+    new("Qux", false),
+    new("Quu1", true),
+    new("Quu3", false),
+    new("Qux2", true),
+};
+
+var featureManager = new FeatureManager();
+featureManager.RegisterFeatures(featureCollection);
+
+while (true)
+{
+    Thread.Sleep(1000);
+    Console.Clear();
+    var table = new Table().Centered();
+    table.AddColumn("Feature name");
+    table.AddColumn("State");
+
+    
+    
+    foreach (var featureName in featureCollection.Select(feature => feature.Name))
+    {
+        
+        table.AddRow(featureName, featureManager.IsFeatureEnabled(featureName) ? "[green]Yes[/]" : "[red]No[/]");
+    }
+
+    table.Border = TableBorder.Rounded;
+    table.BorderColor(Color.Blue);
+    table.Title = new TableTitle("Feature toggles");
+    table.Caption = new TableTitle($"Last updated {DateTime.Now.ToLongTimeString()}");
+
+    AnsiConsole.Write(table);
+}
