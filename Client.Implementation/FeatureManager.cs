951a245752056aaf8e60bb6ea70bc8cd850e2ee2
Lukash Hr <lukasz.hr@outlook.com>
2024-03-21 21:52:49 +0100

    Communication with node blueprint.

--- Client.Implementation/FeatureManager.cs
+++ Client.Implementation/FeatureManager.cs
@@ -1,4 +1,5 @@
-﻿using Wookashi.FeatureSwitcher.Client.Abstraction;
+﻿using System.Text.Json;
+using Wookashi.FeatureSwitcher.Client.Abstraction;
 using Wookashi.FeatureSwitcher.Client.Abstraction.Exceptions;
 using Wookashi.FeatureSwitcher.Client.Implementation.Models;
 
@@ -9,7 +10,7 @@ public class FeatureManager : IFeatureManager
     private List<FeatureStateModel> _features;
     private readonly string _appName;
     private IHttpClientFactory _httpClientFactory;
-    private Uri _nodeAddress = new Uri("http://localhost:5216");
+    private Uri _nodeAddress = new("http://localhost:5216");
 
     public FeatureManager(string appName, List<FeatureStateModel> features, Uri? nodeAddress = null)
     {
@@ -21,14 +22,32 @@ public class FeatureManager : IFeatureManager
         }
     }
 
-    public bool IsFeatureEnabled(string featureName)
+    public async Task<bool> IsFeatureEnabledAsync(string featureName)
     {
         var collectionFeature = _features.FirstOrDefault(feature => feature.Name == featureName);
         if (collectionFeature is null)
         {
             throw new FeatureNotRegisteredException("Feature is not registered in node!");
         }
-        var nodeState = 
-        return collectionFeature.IsEnabled;
+
+        var nodeState = await IsFeatureEnabledOnNode(featureName);
+
+        return collectionFeature.CurrentLocalState;
+    }
+
+    private async Task<bool> IsFeatureEnabledOnNode(string featureName)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var test = new FeatureStateModel("test", true);
+        var response = await httpClient.PostAsync("/feature-state", new StringContent(JsonSerializer.Serialize(test)));
+
+        var model = false;
+        if (response.IsSuccessStatusCode)
+        {
+            var res = response.Content.ReadAsStringAsync().Result;
+            model = JsonSerializer.Deserialize<bool>(res);
+        }
+
+        return model;
     }
 }
