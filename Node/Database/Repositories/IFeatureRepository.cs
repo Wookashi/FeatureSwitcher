49cc8f9a817780b8ae2aa10d73b217c59cdc3420
Lukash Hr <lukasz.hr@outlook.com>
2024-03-27 00:15:09 +0100

    Delete unused files.

--- Node/Database/Repositories/IFeatureRepository.cs
+++
@@ -1,8 +0,0 @@
-using Wookashi.FeatureSwitcher.Node.Database.Entities;
-
-namespace Wookashi.FeatureSwitcher.Node.Database.Repositories;
-
-public interface IFeatureRepository
-{
-    internal List<FeatureEntity> GetFeaturesForApplication(string appName);
-}
--- Node/Dtos/FeatureStateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Wookashi.FeatureSwitcher.Node.Dtos;
-
-internal class FeatureStateDto( string appName, string environment, string featureName, bool initialState)
-{
-    internal string AppName { get; set; } = appName;
-    internal string Environment { get; set; } = environment;
-    internal string FeatureName { get; } = featureName;
-    internal bool InitialState { get; } = initialState;
-}
