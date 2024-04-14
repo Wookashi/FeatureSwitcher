01e67013ce80ebf06d5e82dd85228e4ad9aec5af
Lukash Hr <lukasz.hr@outlook.com>
2024-04-14 16:42:37 +0200

    New app registration model on client.

--- Client/Client.Implementation/Models/AppRegistrationModel.cs
+++ Client/Client.Implementation/Models/AppRegistrationModel.cs
@@ -1,3 +1,13 @@
 namespace Wookashi.FeatureSwitcher.Client.Implementation.Models;
 
-public record AppRegistrationModel(string? ApplicationName, string? Environment, List<FeatureStateModel> Features);
+public sealed class AppRegistrationModel
+{
+    public string AppName { get; set; } = String.Empty;
+    public string Environment { get; set; } = String.Empty;
+    public List<RegisterFeatureStateModel> Features { get; set; } = [];
+    public class RegisterFeatureStateModel(string featureName, bool initialState)
+    {
+        public string FeatureName { get; } = featureName;
+        public bool InitialState { get; } = initialState;
+    }
+}
