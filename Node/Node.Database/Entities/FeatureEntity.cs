7aa95d5be9cbc4eea5c29f14bf8547d9e9f30f52
Lukash Hr <lukasz.hr@outlook.com>
2024-04-15 19:52:06 +0200

    FeatureEntity constructor.

--- Node/Node.Database/Entities/FeatureEntity.cs
+++ Node/Node.Database/Entities/FeatureEntity.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Wookashi.FeatureSwitcher.Node.Database.Entities;
 
 [Index(nameof(Name), IsUnique = true)]
-public sealed class FeatureEntity
+public sealed class FeatureEntity(ApplicationEntity application)
 {
     [Key]
     public int Id { get; set; }
@@ -16,5 +16,5 @@ public sealed class FeatureEntity
     [Required]
     public bool IsEnabled { get; set; }
     
-    public ApplicationEntity? Application { get; set; }
+    public ApplicationEntity Application { get; set; } = application;
 }
