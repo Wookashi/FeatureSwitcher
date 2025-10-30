38393bbd966e93d37e8392c16e5728315065018e
Wookashi <l.hryciuk@cuk.pl>
2025-10-30 15:59:44 +0100

    Check node reachability in console app.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console;
 using Wookashi.FeatureSwitcher.Client.Abstraction;
+using Wookashi.FeatureSwitcher.Client.Abstraction.Exceptions;
 using Wookashi.FeatureSwitcher.Client.Implementation;
 using Wookashi.FeatureSwitcher.Console;
 
 
 var featureCollection = new List<IFeatureStateModel>
 {
-    new ApplicationFeature("Foo",true, new Uri("https://www.wp.pl")),
+    new ApplicationFeature("Foo", true, new Uri("https://www.wp.pl")),
     new ApplicationFeature("Bar", false, new Uri("https://www.wp.pl")),
     new ApplicationFeature("Baz", true, new Uri("https://www.wp.pl")),
     new ApplicationFeature("Qux", false, new Uri("https://www.wp.pl")),
@@ -21,33 +22,39 @@ var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvid
 
 var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
 
-var featureManager = await new FeatureManagerBuilder(new FeatureSwitcherBasicClientConfiguration(
+var featureManagerBuilder = new FeatureManagerBuilder(new FeatureSwitcherBasicClientConfiguration(
         applicationName: "Console",
         environmentName: "testEnv",
         environmentNodeAddress: new Uri("http://localhost:5216")))
     .AddFeatures(featureCollection)
-    .AddHttpClientFactory(httpClientFactory!)
-    .BuildAsync();
+    .AddHttpClientFactory(httpClientFactory!);
 
-while (true)
+try
 {
-    Thread.Sleep(1000);
-    var table = new Table().Centered();
-    table.AddColumn("Feature name");
-    table.AddColumn("State");
-
-    
-    
-    foreach (var featureName in featureCollection.Select(feature => feature.Name))
+    var featureManager = await featureManagerBuilder.BuildAsync();
+    while (true)
     {
-        
-        table.AddRow(featureName, featureManager.IsFeatureEnabledAsync(featureName).Result ? "[green]Yes[/]" : "[red]No[/]");
-    }
+        Thread.Sleep(1000);
+        var table = new Table().Centered();
+        table.AddColumn("Feature name");
+        table.AddColumn("State");
 
-    table.Border = TableBorder.Rounded;
-    table.BorderColor(Color.Blue);
-    table.Title = new TableTitle("Feature toggles");
-    table.Caption = new TableTitle($"Last updated {DateTime.Now.ToLongTimeString()}");
-    Console.Clear();
-    AnsiConsole.Write(table);
+
+        foreach (var featureName in featureCollection.Select(feature => feature.Name))
+        {
+            table.AddRow(featureName, featureManager.IsFeatureEnabledAsync(featureName).Result ? "[green]Yes[/]" : "[red]No[/]");
+        }
+
+        table.Border = TableBorder.Rounded;
+        table.BorderColor(Color.Blue);
+        table.Title = new TableTitle("Feature toggles");
+        table.Caption = new TableTitle($"Last updated {DateTime.Now.ToLongTimeString()}");
+        Console.Clear();
+        AnsiConsole.Write(table);
+    }
+}
+catch (NodeUnreachableException exception)
+{
+    Console.WriteLine("Node unreachable Exception");
+    Console.WriteLine("Exception message: " + exception.Message);
 }
