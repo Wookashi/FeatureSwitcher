3147594ba634ef3c95fe7f5a4d15d9fae5d0c2be
Wookashi <lukasz.hr@outlook.com>
2025-12-08 21:19:54 +0100

    Configure vite.

--- Manager/Web/vite.config.ts
+++ Manager/Web/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
