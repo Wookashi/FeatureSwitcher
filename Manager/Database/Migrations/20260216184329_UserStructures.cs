387ba5f835137efca09e2bf2e8c32878d4739561
Wookashi <l.hryciuk@cuk.pl>
2026-02-17 07:49:56 +0100

    Add migrations to repo and delete claude.md

---
+++ Manager/Database/Migrations/20260216184329_UserStructures.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wookashi.FeatureSwitcher.Manager.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserStructures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "RoleEnum");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoleEnum",
+                table: "Users",
+                newName: "Role");
+        }
+    }
+}
