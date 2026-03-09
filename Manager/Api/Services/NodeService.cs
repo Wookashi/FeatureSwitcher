1f1c192d3d3caf617c051b052af551047b8e3e0f
Wookashi <l.hryciuk@cuk.pl>
2026-03-09 12:57:52 +0100

    Add delete node method.

--- Manager/Api/Services/NodeService.cs
+++ Manager/Api/Services/NodeService.cs
@@ -26,6 +26,12 @@ internal sealed class NodeService
         _nodeRepository
             .CreateOrUpdateNode(nodeRegistrationModel.NodeName, nodeRegistrationModel.NodeAddress);
     }
+    
+    public void DeleteNode(int nodeId)
+    {
+        _nodeRepository
+            .DeleteNode(nodeId);
+    }
 
     public List<NodeDto> GetAllNodes()
     {
